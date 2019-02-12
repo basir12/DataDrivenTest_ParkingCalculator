cd34eea986614013751c3f2ed1df64d1921efec2
basir12 <baseer.wardak@gmail.com>
2019-02-12 08:12:02 -0500

    update DDT page

--- Parking_Calculator/src/test/java/com/Parking_pages/Parking_Calculator.java
+++ Parking_Calculator/src/test/java/com/Parking_pages/Parking_Calculator.java
@@ -11,12 +11,13 @@ import com.ParkingLibrary.Base;
 import com.ParkingLibrary.ExcelManager;
 
 public class Parking_Calculator extends Base {
+	private int counter = 0;
 
 	@DataProvider(name = "parkingcalculator")
 	private Object[][] parkingcalculator() {
-		                                            
-		ExcelManager excelReader = new ExcelManager("src\\test\\resources\\Data\\ParkingCalculator_DDT.xls");
-		
+
+		ExcelManager excelReader = new ExcelManager("src/test/resources/Data/ParkingCalculator_DDT.xls");
+
 		Object[][] data;
 		data = excelReader.getExcelData("parking");
 		return data;
@@ -26,26 +27,32 @@ public class Parking_Calculator extends Base {
 	@BeforeMethod
 	public void parking_Calculator() {
 
-		driver.get("http://adam.goucher.ca/parkcalc/");
-		String webtitle = driver.getTitle();
-		System.out.println("This is Web Title:\t" + webtitle);
-		assertEquals(webtitle, "Parking Calculator");
+		try {
+			driver.get("http://adam.goucher.ca/parkcalc/");
+			String webtitle = driver.getTitle();
+			System.out.println("This is Web Title:\t" + webtitle);
+			assertEquals(webtitle, "Parking Calculator");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	@ Test (dataProvider = "parkingcalculator")
-	public void dataDrivenTest(String lot, String eTime, String eDate, String xTime, String xDate, String expacted ) {
+	@Test(dataProvider = "parkingcalculator")
+	public void dataDrivenTest(String lot, String eTime, String eDate, String xTime, String xDate, String expacted) {
 
 		try {
 
-			myRepository.selectDropDownVisibleText(By.id("Lot"),lot);
+			counter++;
+			myRepository.selectDropDownVisibleText(By.id("Lot"), lot);
 			myRepository.enterTextField(By.id("EntryTime"), eTime);
 			myRepository.enterTextField(By.id("EntryDate"), eDate);
 			myRepository.enterTextField(By.id("ExitTime"), xTime);
 			myRepository.clickButton(By.id("ExitTime"));
 			myRepository.enterTextField(By.id("ExitDate"), xDate);
 			myRepository.clickButton(By.cssSelector("input[name=Submit]"));
-			
-
+			Thread.sleep(2000);
+		
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
